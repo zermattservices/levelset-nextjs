87d72cbdc84982208a8c37c2d715ab662a313d0f
Andrew Dyar <andrew@zermattservices.com>
2025-10-12 18:37:55 -0500

    Add Levelset favicon to the site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,11 @@ import "../components/CodeComponents/RosterTable.css";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider Head={Head}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </PlasmicRootProvider>
   );
