a6f3890052d0752f0084041bdad736849c063acd
Andrew Dyar <andrew@zermattservices.com>
2025-11-07 20:58:40 -0600

    fix: ensure plasmic host uses location context

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
 import { PLASMIC } from '../plasmic-init';
+import { LocationProvider } from '@/components/CodeComponents/LocationContext';
 
 export default function PlasmicHost() {
-  return PLASMIC && <PlasmicCanvasHost />;
+  return (
+    PLASMIC && (
+      <LocationProvider>
+        <PlasmicCanvasHost />
+      </LocationProvider>
+    )
+  );
 }
-
